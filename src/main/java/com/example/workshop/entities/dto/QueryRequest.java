f8cd26561e94558e248539adea1ad8b55be72570
Furkan Deniz <furkandeniz1182@gmail.com>
2022-04-23 18:00:39 +0300

    Added constructor

--- src/main/java/com/example/workshop/entities/dto/QueryRequest.java
+++ src/main/java/com/example/workshop/entities/dto/QueryRequest.java
@@ -4,28 +4,40 @@ public class QueryRequest {
 	private double longtitude;
 	private double latitude;
 	private double radius;
-	
+
 	public QueryRequest() {
 		super();
 	}
+
+	public QueryRequest(double longtitude, double latitude, double radius) {
+		super();
+		this.longtitude = longtitude;
+		this.latitude = latitude;
+		this.radius = radius;
+	}
+
 	public double getLongtitude() {
 		return longtitude;
 	}
+
 	public void setLongtitude(double longtitude) {
 		this.longtitude = longtitude;
 	}
+
 	public double getLatitude() {
 		return latitude;
 	}
+
 	public void setLatitude(double latitude) {
 		this.latitude = latitude;
 	}
+
 	public double getRadius() {
 		return radius;
 	}
+
 	public void setRadius(double radius) {
 		this.radius = radius;
 	}
-	
-	
+
 }
