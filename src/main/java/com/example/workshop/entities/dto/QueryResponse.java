dc6af0a8f3fd7023ead7a1c52bbd4503d1939abd
Furkan Deniz <furkandeniz1182@gmail.com>
2022-04-23 18:00:53 +0300

    Created a new response object

--- src/main/java/com/example/workshop/entities/dto/QueryResponse.java
+++ src/main/java/com/example/workshop/entities/dto/QueryResponse.java
@@ -1,5 +1,25 @@
 package com.example.workshop.entities.dto;
 
+import java.util.List;
+
 public class QueryResponse {
+
+	private List<PlacesDto> places;
+
+	public static QueryResponse of(List<PlacesDto> places) {
+		var response = new QueryResponse();
+		response.setPlaces(places);
+		return response;
+	}
+	
+	public List<PlacesDto> getPlaces() {
+		return places;
+	}
+
+	public void setPlaces(List<PlacesDto> places) {
+		this.places = places;
+	}
+	
+	
 
 }
