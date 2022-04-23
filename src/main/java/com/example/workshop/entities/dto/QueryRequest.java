6a08bd54c4f87f5aeaa4d00db6fddab0bfa514fe
Furkan Deniz <furkandeniz1182@gmail.com>
2022-04-23 16:03:29 +0300

    Created API request-response

---
+++ src/main/java/com/example/workshop/entities/dto/QueryRequest.java
@@ -0,0 +1,31 @@
+package com.example.workshop.entities.dto;
+
+public class QueryRequest {
+	private double longtitude;
+	private double latitude;
+	private double radius;
+	
+	public QueryRequest() {
+		super();
+	}
+	public double getLongtitude() {
+		return longtitude;
+	}
+	public void setLongtitude(double longtitude) {
+		this.longtitude = longtitude;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
+	
+}
---
+++ src/main/java/com/example/workshop/entities/dto/QueryResponse.java
@@ -0,0 +1,5 @@
+package com.example.workshop.entities.dto;
+
+public class QueryResponse {
+
+}
