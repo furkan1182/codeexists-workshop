2aed8956d0e69e1965e0e055660367fcc2a0b710
Furkan Deniz <furkandeniz1182@gmail.com>
2022-04-23 17:59:42 +0300

    Create places entity

--- src/main/java/com/example/workshop/entities/Places.java
+++ src/main/java/com/example/workshop/entities/Places.java
@@ -1,8 +1,79 @@
 package com.example.workshop.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Places {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	@Column
+	private String name;
+	@Column
+	private String address;
+	@Column
+	private double point;
+	@Column
+	private double latitude;
+	@Column
+	private double longtitude;
+
+	public Places() {
+		super();
+	}
+
+	public Places(String name, String address, double point, double latitude, double longtitude) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.point = point;
+		this.latitude = latitude;
+		this.longtitude = longtitude;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getPoint() {
+		return point;
+	}
+
+	public void setPoint(double point) {
+		this.point = point;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongtitude() {
+		return longtitude;
+	}
+
+	public void setLongtitude(double longtitude) {
+		this.longtitude = longtitude;
+	}
 
 }
