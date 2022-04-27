031962be309f78e6017238c69c2a6a82b9a86308
Furkan Deniz <furkandeniz1182@gmail.com>
2022-04-27 21:04:03 +0300

    made call to Google Places API

--- src/main/java/com/example/workshop/service/GooglePlacesClient.java
+++ src/main/java/com/example/workshop/service/GooglePlacesClient.java
@@ -1,17 +1,47 @@
 package com.example.workshop.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.workshop.entities.Places;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 @Service
 public class GooglePlacesClient {
-	public List<Places> search(double longtitude, double latitude, double radius) {
-		return List.of(new Places("Bedirhan Pub", "Bahçelievler", 4.0, 27.0, 35.1),
-				new Places("Kebabçı Ali", "Bahçelievler", 5.0, 27.0, 35.2),	
-				new Places("Tatlıcı Nuri", "Bahçelievler", 3.2, 27.0, 35.3),
-				new Places("Özen Butik", "Bahçelievler", 2.4, 27.0, 35.4));
+	@Value("${google.places.api.key}")
+	private String apiKey;
+
+	private static final String BASE_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+
+	public List<Places> search(double longtitude, double latitude, double radius) throws IOException {
+		OkHttpClient client = new OkHttpClient().newBuilder().build();
+		Request request = new Request.Builder().url(BASE_URL + "location=" + longtitude + "%2C" + latitude + "&radius="
+				+ (int) radius + "&type=restaurant&keyword=cruise&key=" + apiKey).method("GET", null).build();
+
+		var places = new ArrayList<Places>();
+		Response response = client.newCall(request).execute();
+
+		if (response.isSuccessful()) {
+			JSONObject json = new JSONObject(response.body().string());
+			JSONArray results = (JSONArray) json.get("results");
+			for (Object result : results) {
+				JSONObject placeObj = (JSONObject) result;
+				places.add(new Places(placeObj.getString("name"), placeObj.getString("vicinity"),
+						placeObj.getDouble("rating"),
+						placeObj.getJSONObject("geometry").getJSONObject("location").getDouble("lat"),
+						placeObj.getJSONObject("geometry").getJSONObject("location").getDouble("lng")));
+			}
+		}
+
+		return places;
 	}
 }
